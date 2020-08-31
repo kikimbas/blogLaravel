56026d694452027efffaf14c4370867eee6f86b6
kikimbas <sergey.dev91@gmail.com>
2020-08-31 15:44:26 +0300

    add new files

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,15 +9,18 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
+    <!-- Scripts
     <script src="{{ asset('js/app.js') }}" defer></script>
-
+-->
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-    <!-- Styles -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Styles
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    -->
 </head>
 <body>
     <div id="app">
