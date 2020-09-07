fa4ee8e939bfa0153a93ca891224bd35160ceb12
kikimbas <sergey.dev91@gmail.com>
2020-09-07 13:25:11 +0300

    'add'

--- routes/web.php
+++ routes/web.php
@@ -10,5 +10,13 @@ Route::get('/', 'HomeController@index')->name('home');
 //});
 
 Route::get('/profile', 'ProfileController@index')->name('profile-view');
+//Route::get('/post/view_form', 'PostController@index')->name('view_form');
+Route::get('/post/view_form', function () {
+    return view('post/view_form');
+})->name('view_form');
+
+Route::get('/post/list_post', 'PostController@listOfPosts')->name('list_post');
+
+Route::post('/post/add_post', 'PostController@add')->name('add_post');
 Route::post('/profile/edit', 'ProfileController@edit')->name('profile-edit');
 
